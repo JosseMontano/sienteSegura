7cdbdbe72782a3a343444c9437746a72aa0b6022
JosseMontano <josemariazambranamontano123@gmail.com>
2023-06-23 15:00:32 -0400

    add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Route, Routes, HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path={"/"} element={<App />} />
+        <Route path={"/login"} element={<App />} />
+
+        <Route path="*" element={<p>Page no found</p>} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
-)
+);
